1d1a6073f3e2d3610af86a95997e2dc2e8356761
root <root@core-ssh.local.hass.io>
2020-11-29 15:43:33 -0500

    tweak script

--- data/run.sh
+++ data/run.sh
@@ -2,8 +2,13 @@
 
 bashio::log.info "Starting SCP copy..."
 
+files=`find /backup -name *.tar -maxdepth 1 -mtime -1`
+
+bashio::log.info $files 
+
 exec /usr/bin/scp -i "$(bashio::config 'private_key')" -o \
 	StrictHostKeyChecking=no \
-	`find /backup -name *.tar -maxdepth 1 -mtime -1` $(bashio::config 'remote_user')@$(bashio::config 'remote_host'):$(bashio::config 'remote_path')
+	$files \
+	$(bashio::config 'remote_user')@$(bashio::config 'remote_host'):"$(bashio::config 'remote_path')"
 
 bashio::log.info "SCP copy complete..."
